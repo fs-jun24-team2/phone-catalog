65254aebdc4e3323ec708a8c63cae5d0472b47fa
Andrii Zakharenko <82187654+AndriiZakharenko@users.noreply.github.com>
2024-09-14 11:32:39 +0300

    add CSS vars, rest (#2)
    * add CSS vars, reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import './App.css';
+import './style.scss';
 
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <div className="header__container">HEADER</div>
+      </header>
+
+      <main className="main">
+        <div className="main__container">MAIN</div>
+      </main>
+
+      <footer className="footer">
+        <div className="footer__container">FOOTER</div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { store } from './app/store';
 import App from './App.tsx';
-import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
