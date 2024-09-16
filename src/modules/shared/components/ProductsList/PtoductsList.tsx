0b0b9bb6445beef4818174f9bd16ad12ed9504b4
Oleksii Kislukhin <kislukhin.lesha@gmail.com>
2024-09-16 12:13:14 +0300

    Add Card layout

--- src/modules/shared/components/ProductsList/PtoductsList.tsx
+++ src/modules/shared/components/ProductsList/PtoductsList.tsx
@@ -1,7 +1,8 @@
 //import styles from './ProductsList.module.scss';
 
 import { ProductCard } from '../ProductCard';
+import { Phone } from '../../../../types/Phone';
 
 export const ProductsList = () => {
-  return <ProductCard />;
+  return <ProductCard product={{} as Phone} />;
 };
