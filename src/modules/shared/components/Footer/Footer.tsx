f9a1009104e539fa77fbe89b27681eae5947a05a
Iryna Yehorova <yehorova.dev@gmail.com>
2024-09-16 18:38:37 +0300

    add pre-deploy (#13)

--- src/modules/shared/components/Footer/Footer.tsx
+++ src/modules/shared/components/Footer/Footer.tsx
@@ -13,9 +13,27 @@ export const Footer = () => {
       <div className="footer__container foo">
         <img src="images/footer/logo.svg" alt="Logo" />
         <div className={styles['foo__link']}>
-          <a href="https://github.com/fs-jun24-team2/phone-catalog" target="_blank" rel="noopener noreferrer">Github</a>
-          <a href="https://github.com/fs-jun24-team2/phone-catalog" target="_blank" rel="noopener noreferrer">Contacts</a>
-          <a href="https://github.com/fs-jun24-team2/phone-catalog" target="_blank" rel="noopener noreferrer">Rights</a>
+          <a
+            href="https://github.com/fs-jun24-team2/phone-catalog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://github.com/fs-jun24-team2/phone-catalog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contacts
+          </a>
+          <a
+            href="https://github.com/fs-jun24-team2/phone-catalog"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rights
+          </a>
         </div>
         <div className={styles['foo__go-back-btn']} onClick={scrollToSection}>
           <div>Back to top</div>
