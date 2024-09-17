20b6b3baf98b07ce2e84bdca113712f41de80bb1
Iryna Yehorova <yehorova.dev@gmail.com>
2024-09-17 23:51:14 +0300

    bugfix: fix lint and build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/phone-catalog/',
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/images',
+          dest: path.resolve(__dirname, '/')
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
