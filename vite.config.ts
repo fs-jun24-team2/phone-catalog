492f9108b46a859f79f328aad7f534639e077f10
Iryna Yehorova <yehorova.dev@gmail.com>
2024-09-18 00:34:52 +0300

    bugfix: fix github build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/images',
-          dest: path.resolve(__dirname, '/')
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@root': path.resolve(__dirname, './')
+      '@root': path.resolve(__dirname, './'),
     },
   },
-})
+});
