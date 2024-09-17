6e689853fb972d2fc9ca80c94315a0b2d31b2ea1
Iryna Yehorova <yehorova.dev@gmail.com>
2024-09-18 00:39:15 +0300

    bugfix: fix lint and build error (#20)
    * bugfix: fix lint and build error
    
    * bugfix: fix github build error

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,12 @@ import './style.scss';
 import { Footer } from './modules/shared/components/Footer';
 import { Header } from './modules/shared/components/Header';
 
-// import { ProductsList } from './modules/shared/components/ProductsList';
-
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <main className="main">
         <div className="main__container">
-         
-
           <Outlet />
         </div>
       </main>
