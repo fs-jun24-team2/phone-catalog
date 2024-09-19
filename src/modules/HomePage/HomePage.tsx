7584b3a19ea1d929ccd7b7f8dc7316bf9be29c0f
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-09-20 00:24:35 +0300

    feat: lang (i18n)

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 // import styles from './HomePage.module.scss';
 
 import { useAppSelector } from '@/app/hooks';
+import { useTranslation } from 'react-i18next';
 import { PictureSlider } from './components/PictureSlider';
 import { ProductsSlider } from './components/ProductsSlider';
 import { ShopByCategory } from './components/ShopByCategory';
@@ -8,24 +9,27 @@ import { ShopByCategory } from './components/ShopByCategory';
 export const HomePage = () => {
   const products = useAppSelector(state => state.products);
   const phonePlaceholders = Object.values(products.phones).slice(0, 10);
-
+  const { t } = useTranslation();
   return (
     <>
       <div style={{ paddingTop: '100px' }}></div>
       {/* hidden */}
-      <h1 style={{ paddingTop: '100px' }}>Product Catalog</h1>
+      <h1 style={{ paddingTop: '100px' }}>{t('home')}</h1>
 
       {/* Banner */}
       <PictureSlider />
 
       {/* Section brand new models */}
-      <ProductsSlider title="Brand new models" products={phonePlaceholders} />
+      <ProductsSlider
+        title={t('brand_new_models')}
+        products={phonePlaceholders}
+      />
 
       {/* Shop by category block with links to /phones, /tablets, and /accessories. */}
       <ShopByCategory />
 
       {/* Section hot prices */}
-      <ProductsSlider title="Brand hot prices" products={phonePlaceholders} />
+      <ProductsSlider title={t('hot_prices')} products={phonePlaceholders} />
       <div></div>
     </>
   );
