929aa4feb94997a3a4786d23ea86205119c4991b
Olga Brendak <olga.brendak.dg@gmail.com>
2024-09-20 01:00:26 +0300

    feat: add gallery

--- src/modules/ProductDetailsPage/Gallery/Gallery.tsx
+++ src/modules/ProductDetailsPage/Gallery/Gallery.tsx
@@ -1,5 +1,34 @@
-// import styles from './Gallery.module.scss';
+import styles from './Gallery.module.scss';
+import React, { useState } from 'react';
 
-export const Gallery = () => {
-  return <></>;
+interface ImageGalleryProps {
+  images: string[];
+}
+
+export const Gallery: React.FC<ImageGalleryProps> = ({ images }) => {
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  const handleImageClick = (image: string) => {
+    setSelectedImage(image);
+  };
+
+  return (
+    <div className={styles['gallery']}>
+      <div className={styles['gallery__miniatures']}>
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Miniature ${index}`}
+            className={`${styles['gallery__miniatures__miniature']} ${selectedImage === image ? styles['active'] : ''}`}
+            onClick={() => handleImageClick(image)}
+          />
+        ))}
+      </div>
+
+      <div className={styles['gallery__main-image']}>
+        <img src={selectedImage} alt="Selected" />
+      </div>
+    </div>
+  );
 };
