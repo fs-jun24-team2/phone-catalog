1adfbc9b85ac309362d7f581309ffac5c1fceaaa
Olga Brendak <olga.brendak.dg@gmail.com>
2024-09-19 18:10:12 +0300

    feat: add breadcrumbs

--- src/modules/shared/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/modules/shared/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,44 @@
-// import styles from './Breadcrumbs.module.scss';
+import styles from './Breadcrumbs.module.scss';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Breadcrumbs = () => {
-  return <></>;
+  const location = useLocation();
+  let currentLink = '';
+
+  const upFirstLetterOfTheWord = (word: string) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+
+  const crumbs = location.pathname
+    .split('/')
+    .filter(crumb => crumb !== '')
+    .map((crumb, index, array) => {
+      currentLink += `/${crumb}`;
+
+      return (
+        <div
+          className={
+            index === array.length - 1
+              ? styles.breadcrumbs__crumbActive
+              : styles.breadcrumbs__crumb
+          }
+          key={crumb}
+        >
+          <Link to={currentLink}>{upFirstLetterOfTheWord(crumb)}</Link>
+          {index < array.length - 1 && (
+            <span className={styles.separator}></span>
+          )}
+        </div>
+      );
+    });
+
+  return (
+    <div className={styles.breadcrumbs}>
+      <Link className={styles.breadcrumbs__crumb} to="/">
+        <img className={styles.icon} src="images/original/breadcrumbs/home.svg" />
+      </Link>
+      <span className={styles.separator}></span>
+      {crumbs}
+    </div>
+  );
 };
