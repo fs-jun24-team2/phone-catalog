90142fd6f54ae9d1b170b9518b9dab578a94f90f
Ivan Opar <vanya.opar@gmail.com>
2024-09-20 17:09:03 +0300

    feat: about section

--- src/modules/ProductDetailsPage/About/About.tsx
+++ src/modules/ProductDetailsPage/About/About.tsx
@@ -1,5 +1,28 @@
-// import styles from './About.module.scss';
+import React from 'react';
+import styles from './About.module.scss';
+import { PhoneDescription } from '@/types/Product';
 
-export const About = () => {
-  return <></>;
+interface AboutProps {
+  description: PhoneDescription[];
+}
+
+export const About: React.FC<AboutProps> = ({ description }) => {
+  return (
+    <section className={styles.about}>
+      <h2 className={styles.about__title}>About</h2>
+      {description.map((item, index) => (
+        <div key={index} className={styles.about__text_container}>
+          <h3 className={styles.about__text_title}>{item.title}</h3>
+          {item.text.map((paragraph, idx) => (
+            <div>
+              <p key={idx} className={styles.about__text}>
+                {paragraph}
+              </p>
+              <br />
+            </div>
+          ))}
+        </div>
+      ))}
+    </section>
+  );
 };
