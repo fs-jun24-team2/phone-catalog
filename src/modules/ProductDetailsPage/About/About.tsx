05ce54bb29b27e2ac295e4cbae3ef9057c894f17
Ivan Opar <vanya.opar@gmail.com>
2024-09-21 21:20:21 +0300

    feat: fix about section

--- src/modules/ProductDetailsPage/About/About.tsx
+++ src/modules/ProductDetailsPage/About/About.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './About.module.scss';
 import { PhoneDescription } from '@/types/Product';
+import { AboutItem } from './AboutItem';
 
 interface AboutProps {
   description: PhoneDescription[];
@@ -11,17 +12,7 @@ export const About: React.FC<AboutProps> = ({ description }) => {
     <section className={styles.about}>
       <h2 className={styles.about__title}>About</h2>
       {description.map((item, index) => (
-        <div key={index} className={styles.about__text_container}>
-          <h3 className={styles.about__text_title}>{item.title}</h3>
-          {item.text.map((paragraph, idx) => (
-            <div>
-              <p key={idx} className={styles.about__text}>
-                {paragraph}
-              </p>
-              <br />
-            </div>
-          ))}
-        </div>
+        <AboutItem key={index} item={item} />
       ))}
     </section>
   );
