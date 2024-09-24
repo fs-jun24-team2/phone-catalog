7ce31c5a55c4bdc4a548b50beed7a18a5361e134
Ivan Opar <vanya.opar@gmail.com>
2024-09-24 13:05:00 +0300

    add: notFoundProduct, switch language, fix cursors (#67)
    * add: notFoundProduct, switch language, fix cursors
    
    * fix img

--- src/modules/ProductsPage/SortAndPagination/SortAndPagination.tsx
+++ src/modules/ProductsPage/SortAndPagination/SortAndPagination.tsx
@@ -3,6 +3,7 @@ import styles from './SortAndPagination.module.scss';
 import { Product } from '@/types/Product';
 import cn from 'classnames';
 import { SortSelector } from './SortSelector/SortSelector';
+import { useTranslation } from 'react-i18next';
 
 interface SortAndPaginationPanelProps {
   products: Product[];
@@ -11,27 +12,29 @@ interface SortAndPaginationPanelProps {
 export const SortAndPaginationPanel: React.FC<
   SortAndPaginationPanelProps
 > = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={cn('grid-container', [styles.selectors])}>
-      {/*сортування */}
+      {/* Сортування */}
       <SortSelector
-        label="Sort by:"
+        label={t('sortBy')}
         options={[
-          { value: 'cheapest', label: 'Cheapest' },
-          { value: 'alphabetically', label: 'Alphabetically' },
-          { value: 'age', label: 'Newest' },
+          { value: 'cheapest', label: t('cheapest') },
+          { value: 'alphabetically', label: t('alphabetically') },
+          { value: 'age', label: t('newest') },
         ]}
         className={styles.selectors__sort}
       />
 
-      {/*елементи на сторінці*/}
+      {/* Елементи на сторінці */}
       <SortSelector
-        label="Items per page:"
+        label={t('itemsPerPage')}
         options={[
           { value: '4', label: '4' },
           { value: '8', label: '8' },
           { value: '16', label: '16' },
-          { value: 'all', label: 'All' },
+          { value: 'all', label: t('all') },
         ]}
         className={styles.selectors__pagination}
       />
