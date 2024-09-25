b428980b6f5cb3e6d46bfa001395d656ce4b631d
Ivan Opar <vanya.opar@gmail.com>
2024-09-25 16:00:41 +0300

    stap 1: fix skeleton

--- src/modules/shared/components/Skeletons/BreadcrumbsSkeleton.tsx
+++ src/modules/shared/components/Skeletons/BreadcrumbsSkeleton.tsx
@@ -1,13 +1,23 @@
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import styles from '../Breadcrumbs/Breadcrumbs.module.scss';
 
 export const BreadcrumbsSkeleton = () => {
   return (
-    <div className={styles['breadcrumbs']}>
-      <Skeleton circle height={16} width={16} style={{ marginRight: '13px' }} />
-      <Skeleton width={80} height={16} style={{ marginRight: '13px' }} />
-      <Skeleton width={60} height={16} />
-    </div>
+    <SkeletonTheme
+      baseColor="rgba(128, 128, 128, 0.1)"
+      highlightColor="rgba(135, 206, 250, 0.25)"
+    >
+      <div className={styles['breadcrumbs']}>
+        <Skeleton
+          circle
+          height={16}
+          width={16}
+          style={{ marginRight: '13px' }}
+        />
+        <Skeleton width={80} height={16} style={{ marginRight: '13px' }} />
+        <Skeleton width={60} height={16} />
+      </div>
+    </SkeletonTheme>
   );
 };
