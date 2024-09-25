1ebf20053aa5d7b8a3e90699b6058a6c9a68fb4f
Ivan Opar <vanya.opar@gmail.com>
2024-09-25 17:44:28 +0300

    stap 1: fix skeleton (#88)

--- src/modules/shared/components/Skeletons/PaginationSkeleton.tsx
+++ src/modules/shared/components/Skeletons/PaginationSkeleton.tsx
@@ -1,22 +1,27 @@
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import styles from '../../components/Pagination/Pagination.module.scss';
 
 export const PaginationSkeleton = () => {
   return (
-    <div className={styles.pagination}>
-      <Skeleton height={40} width={40} className={styles.arrow} />
+    <SkeletonTheme
+      baseColor="rgba(128, 128, 128, 0.1)"
+      highlightColor="rgba(135, 206, 250, 0.25)"
+    >
+      <div className={styles.pagination}>
+        <Skeleton height={40} width={40} className={styles.arrow} />
 
-      {[...Array(4)].map((_, index) => (
-        <Skeleton
-          key={index}
-          height={40}
-          width={40}
-          style={{ margin: '0 8px' }}
-        />
-      ))}
+        {[...Array(4)].map((_, index) => (
+          <Skeleton
+            key={index}
+            height={40}
+            width={40}
+            style={{ margin: '0 8px' }}
+          />
+        ))}
 
-      <Skeleton height={40} width={40} className={styles.arrow} />
-    </div>
+        <Skeleton height={40} width={40} className={styles.arrow} />
+      </div>
+    </SkeletonTheme>
   );
 };
--- src/modules/shared/components/Skeletons/ProductCardSkeleton.tsx
+++ src/modules/shared/components/Skeletons/ProductCardSkeleton.tsx
@@ -1,28 +1,37 @@
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import styles from '../ProductCard/ProductCard.module.scss';
 
 export const ProductCardSkeleton = () => {
   return (
-    <article className={styles['product-card']}>
-      <div className={styles['product-card__header']}>
-        <Skeleton height={196} width="100%" />
-        <Skeleton height={21} width={150} style={{ marginTop: '16px' }} />{' '}
-      </div>
-      <div className={styles['product-card__prices-container']}>
-        <Skeleton width={100} height={31} />
-        <Skeleton width={100} height={28} />
-      </div>
-      <div className={styles['product-card__devider']} />
-      <div className={styles['product-card__specs-container']}>
-        <Skeleton height={20} width="100%" />
-        <Skeleton height={20} width="100%" />
-        <Skeleton height={20} width="100%" />
-      </div>
-      <div className={styles['product-card__button-container']}>
-        <Skeleton height={40} width={150} />
-        <Skeleton height={40} width={40} />
-      </div>
-    </article>
+    <SkeletonTheme
+      baseColor="rgba(128, 128, 128, 0.1)"
+      highlightColor="rgba(135, 206, 250, 0.25)"
+    >
+      <article className={styles['product-card']}>
+        <div className={styles['product-card__header']}>
+          <Skeleton height={196} width="100%" />
+          <Skeleton
+            height={21}
+            width={150}
+            style={{ marginTop: '16px' }}
+          />{' '}
+        </div>
+        <div className={styles['product-card__prices-container']}>
+          <Skeleton width={100} height={31} />
+          <Skeleton width={100} height={28} />
+        </div>
+        <div className={styles['product-card__devider']} />
+        <div className={styles['product-card__specs-container']}>
+          <Skeleton height={20} width="100%" />
+          <Skeleton height={20} width="100%" />
+          <Skeleton height={20} width="100%" />
+        </div>
+        <div className={styles['product-card__button-container']}>
+          <Skeleton height={40} width={150} />
+          <Skeleton height={40} width={40} />
+        </div>
+      </article>
+    </SkeletonTheme>
   );
 };
