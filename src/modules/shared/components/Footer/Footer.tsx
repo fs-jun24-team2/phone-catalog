666f1721c0d5f59472b7eb98b3a5120bf901fd43
Olga Brendak <olga.brendak.dg@gmail.com>
2024-09-25 19:53:14 +0300

    feat: add dark theme in footer

--- src/modules/shared/components/Footer/Footer.tsx
+++ src/modules/shared/components/Footer/Footer.tsx
@@ -1,9 +1,8 @@
-// Footer.tsx
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 import footer_logo from '/images/original/footer/logo.svg';
 import { scrollToTop } from '../../helpers/scrollToTop';
+import { useEffect, useState } from 'react';
 
 export const Footer = () => {
   const { t } = useTranslation();
@@ -13,31 +12,53 @@ export const Footer = () => {
       label: 'Github',
     },
     {
-      href: '/contacts',
+      href: 'https://github.com/fs-jun24-team2/phone-catalog',
       label: t('contacts'),
     },
     {
-      href: '/rights',
+      href: 'https://github.com/fs-jun24-team2/phone-catalog',
       label: t('rights'),
     },
   ];
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const checkTheme = () => {
+      const isDark = document.body.classList.contains('dark_theme');
+      setIsDarkTheme(isDark);
+    };
+
+    checkTheme();
+
+    const observer = new MutationObserver(checkTheme);
+    observer.observe(document.body, { attributes: true });
+
+    return () => observer.disconnect();
+  });
 
   return (
     <footer>
-      <div className={styles['footer']}>
-        <img src={footer_logo} alt="Logo" />
+      <div
+        className={`${styles['footer']} ${isDarkTheme ? styles['footer-dark'] : ''}`}
+      >
+        <img
+          src={
+            isDarkTheme
+              ? footer_logo
+              : '/public/images/original/footer/logo-black.svg'
+          }
+          alt="Logo"
+        />
         <div className={styles['footer__link']}>
           {links.map((link, index) => (
-            <Link
+            <a
               key={index}
-              to={link.href}
-              target={link.href.startsWith('http') ? '_blank' : undefined}
-              rel={
-                link.href.startsWith('http') ? 'noopener noreferrer' : undefined
-              }
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
             >
               {link.label}
-            </Link>
+            </a>
           ))}
         </div>
         <div className={styles['footer__go-back-btn']} onClick={scrollToTop}>
