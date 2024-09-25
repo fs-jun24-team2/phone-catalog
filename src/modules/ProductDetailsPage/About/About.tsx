13836481d2d67cdb79bce7682920c7a008b696b1
Olga Brendak <olga.brendak.dg@gmail.com>
2024-09-26 02:21:36 +0300

    feat: add dark theme to about and techSpecs

--- src/modules/ProductDetailsPage/About/About.tsx
+++ src/modules/ProductDetailsPage/About/About.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './About.module.scss';
 import { PhoneDescription } from '@/types/Product';
 import { AboutItem } from './AboutItem';
@@ -10,10 +10,27 @@ interface AboutProps {
 
 export const About: React.FC<AboutProps> = ({ description }) => {
   const { t } = useTranslation();
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const checkTheme = () => {
+      const isDark = document.body.classList.contains('dark_theme');
+      setIsDarkTheme(isDark);
+    };
+
+    checkTheme();
+
+    const observer = new MutationObserver(checkTheme);
+    observer.observe(document.body, { attributes: true });
+
+    return () => observer.disconnect();
+  });
 
   return (
-    <section className={styles.about}>
-      <h2 className={styles.about__title}>{t('about')}</h2>
+    <section
+      className={`${styles['about']} ${isDarkTheme ? styles['about-dark'] : ''}`}
+    >
+      <h2 className={styles['about__title']}>{t('about')}</h2>
       {description.map((item, index) => (
         <AboutItem key={index} item={item} />
       ))}
