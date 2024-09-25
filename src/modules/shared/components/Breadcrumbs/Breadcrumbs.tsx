2e6a3558fbee47d4611b7213f57b812ee9d30f57
Olga Brendak <olga.brendak.dg@gmail.com>
2024-09-26 00:35:29 +0300

    feat: add breadcrumbs dark theme

--- src/modules/shared/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/modules/shared/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,18 +1,38 @@
 import { Link, useLocation } from 'react-router-dom';
 import styles from './Breadcrumbs.module.scss';
 import { Crumb } from './Crumb/Crumb';
+import breadcrumbs_home from '/images/original/breadcrumbs/home.svg';
+import breadcrumbs_dark_home from '/images/original/breadcrumbs/homedark.svg';
+import { useEffect, useState } from 'react';
 
 export const Breadcrumbs = () => {
   const location = useLocation();
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const checkTheme = () => {
+      const isDark = document.body.classList.contains('dark_theme');
+      setIsDarkTheme(isDark);
+    };
+
+    checkTheme();
+
+    const observer = new MutationObserver(checkTheme);
+    observer.observe(document.body, { attributes: true });
+
+    return () => observer.disconnect();
+  });
 
   const crumbs = location.pathname.split('/').filter(crumb => crumb !== '');
 
   return (
-    <div className={styles['breadcrumbs']}>
+    <div
+      className={`${styles['breadcrumbs']} ${isDarkTheme ? styles['breadcrumbs-dark'] : ''}`}
+    >
       <Link to="/">
         <img
           className={styles['breadcrumbs__icon-home']}
-          src="images/original/breadcrumbs/home.svg"
+          src={isDarkTheme ? breadcrumbs_dark_home : breadcrumbs_home}
           alt="Home"
         />
       </Link>
